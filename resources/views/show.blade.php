aae438d7261c4542d54965fe3c6587dcfbedc1b5
rogerlenhart <rogerlenhart15@gmail.com>
2021-03-05 14:43:40 -0300

    V1.3 - Melhoria na interface do show

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,9 +6,19 @@
 
 @section('content')
   <div class="col-8 m-auto">
-    Nome: {{$cliente->nome}} <br>
-    Idade: {{$cliente->idade}} <br>
-    Cidade: {{$cliente->cidade}}
+    <div class="card m-auto" style="width: 24rem;">
+      <div class="card-header bg-dark text-white">
+        <h5 class="card-title "><i class="fa fa-vcard-o" style="font-size:1.25rem"></i>  CLIENTE Nº {{$cliente->id}}</h5>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item"><b>Nome: </b>{{$cliente->nome}}</li>
+        <li class="list-group-item"><b>Idade: </b>{{$cliente->idade}}</li>
+        <li class="list-group-item"><b>Cidade: </b>{{$cliente->cidade}}</li>
+      </ul>
+    </div>
+     <br>
+     <br>
+    
   </div>
 
 @endsection
--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Teste Laravel</title>
     <link rel="stylesheet" href="{{url('assets/bootstrap/css/bootstrap.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 <div class="container-fluid">
